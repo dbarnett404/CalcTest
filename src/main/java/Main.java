b48ca4e3ba94b6d7156ab27dfaece0c0510d61a0
dbarnett404 <dbarnett@studygroup.com>
2020-09-16 14:43:16 +0100

    Changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +14,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calc Test");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter a number");
+        int num1 = myObj.nextInt();
     }
 }
